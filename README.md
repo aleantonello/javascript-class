dbfe5415cd1f1b22a2373e7f61ec4a7279374ab7
Alessandro Antonello <alessandro.antonello@atipica.ong.br>
2021-07-31 18:25:59 -0300

    Aula de matrizes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # javascript-class
-Aulas de Javascript
+
+Repositório para aulas de Javascript.
+
+Repositório original está no [Github](https://github.com/aleantonello/javascript-class).
+
---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Aula sobre matrizes!");
+let matriz = [
+    'Hora',
+    'Minuto',
+    'Segundo'
+];
+
+console.log(matriz[0]);
+console.log("A matriz possui " + matriz.length + " elementos.");
+
+let criarObjeto = { };
+let obj = {
+    nome: "Alessandro",
+    idade: 49,
+    estaVelho() {
+        return true;
+    }
+};
+console.log(obj.nome + ' tem ' + obj.idade + ' anos de idade.');
